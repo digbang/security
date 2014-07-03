222336de2294be251dfe05591e56e9f4a56d9644
gcontreras <gcontreras@digbang.com>
2014-07-03 13:35:18 -0300

    Comment updated

--- src/Digbang/Security/Contracts/PermissionRepositoryInterface.php
+++ src/Digbang/Security/Contracts/PermissionRepositoryInterface.php
@@ -4,19 +4,19 @@ interface PermissionRepositoryInterface
 {
 	/**
 	 * @param  string $route
-	 * @return string The permission matching the url, if it needs one.
+	 * @return string The permission matching the route, if it needs one.
 	 */
 	public function getForRoute($route);
 
 	/**
 	 * @param  string $action
-	 * @return string The permission matching the url, if it needs one.
+	 * @return string The permission matching the action, if it needs one.
 	 */
 	public function getForAction($action);
 
 	/**
 	 * @param  string $path
-	 * @return string The permission matching the url, if it needs one.
+	 * @return string The permission matching the path, if it needs one.
 	 */
 	public function getForPath($path);
 }
