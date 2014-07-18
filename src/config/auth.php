11851bd19987cd94008beecefb467193c69949b6
gcontreras <gcontreras@digbang.com>
2014-07-18 17:22:44 -0300

    Overriden Sentry config file

---
+++ src/config/auth.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Wrapper around Sentry config with some default configurations.
+ * For full documentation, see the sentry manual
+ * @see https://cartalyst.com/manual/sentry
+ */
+return [
+	'hasher' => 'bcrypt',
+	'cookie' => [
+		'key' => 'db_backoffice_'
+	],
+	'groups' => [
+		'model' => 'Cartalyst\Sentry\Groups\Eloquent\Group'
+	],
+	'users'  => [
+		'model'           => 'Cartalyst\Sentry\Users\Eloquent\User',
+		'login_attribute' => 'email'
+	],
+	'user_groups_pivot_table' => 'user_group',
+	'throttling' => [
+		'enabled'         => false,
+		'model'           => 'Cartalyst\Sentry\Throttling\Eloquent\Throttle',
+		'attempt_limit'   => 5,
+		'suspension_time' => 15
+	]
+];
