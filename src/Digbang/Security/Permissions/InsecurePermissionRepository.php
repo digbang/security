ce4a726b5bc60a38e6fd2932db36d59d3c3e8a2b
gcontreras <gcontreras@digbang.com>
2014-08-13 10:51:40 -0300

    First implementation of the PermissionRepository: InsecurePermissionRepository.

---
+++ src/Digbang/Security/Permissions/InsecurePermissionRepository.php
@@ -0,0 +1,15 @@
+<?php namespace Digbang\Security\Permissions;
+
+class InsecurePermissionRepository implements PermissionRepository
+{
+	public function getForRoute($route) { return null; }
+
+	public function getForAction($action) { return null; }
+
+	public function getForPath($path) { return null; }
+
+	public function all()
+	{
+		return [];
+	}
+}
