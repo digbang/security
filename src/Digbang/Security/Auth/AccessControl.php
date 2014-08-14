5772fd140332714eb4bc41bf169f42f8f6b45e97
gcontreras <gcontreras@digbang.com>
2014-08-14 12:11:56 -0300

    Access control object, a proxy over Sentry

---
+++ src/Digbang/Security/Auth/AccessControl.php
@@ -0,0 +1,51 @@
+<?php namespace Digbang\Security\Auth;
+
+use Cartalyst\Sentry\Sentry;
+
+class AccessControl
+{
+	/**
+	 * @var \Cartalyst\Sentry\Sentry
+	 */
+	protected $sentry;
+
+	function __construct(Sentry $sentry)
+	{
+		$this->sentry = $sentry;
+	}
+
+	public function authenticate($email, $password, $remember = false)
+    {
+	    return $this->sentry->authenticate(['login' => $email, 'password' => $password], $remember);
+    }
+
+    public function logout()
+    {
+        $this->sentry->logout();
+    }
+
+    public function getUser()
+    {
+        return $this->sentry->getUser();
+    }
+
+    public function resetPassword($code, $newPassword)
+    {
+        if ($user = $this->sentry->findUserByResetPasswordCode($code))
+        {
+	        return $user->attemptResetPassword($code, $newPassword);
+        }
+
+	    return false;
+    }
+
+    public function activate($code)
+    {
+        if ($user = $this->sentry->findUserByActivationCode($code))
+        {
+	        return $user->attemptActivation($code);
+        }
+
+	    return false;
+    }
+}
