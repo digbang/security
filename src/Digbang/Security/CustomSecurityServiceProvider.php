0f3ad3213aa74606f4c75b8b3bb9b9f9edacc47b
Guido Contreras Woda <gcontreras@digbang.com>
2015-03-11 12:30:53 -0300

    Missing namespace for hashers

--- src/Digbang/Security/CustomSecurityServiceProvider.php
+++ src/Digbang/Security/CustomSecurityServiceProvider.php
@@ -1,170 +1,181 @@
 <?php namespace Digbang\Security;
 
+use Cartalyst\Sentry\Hashing;
 use Cartalyst\Sentry\Cookies\IlluminateCookie;
 use Cartalyst\Sentry\Sentry;
 use Cartalyst\Sentry\Sessions\IlluminateSession;
-use Cartalyst\Sentry\Users\ProviderInterface      as UserProvider;
-use Cartalyst\Sentry\Groups\ProviderInterface     as GroupProvider;
+use Cartalyst\Sentry\Users\ProviderInterface as UserProvider;
+use Cartalyst\Sentry\Groups\ProviderInterface as GroupProvider;
 use Cartalyst\Sentry\Throttling\ProviderInterface as ThrottleProvider;
+use Illuminate\Container\Container;
 use Illuminate\Support\ServiceProvider;
 
 class CustomSecurityServiceProvider extends ServiceProvider
 {
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->registerHasher();
-        $this->registerUserProvider();
-        $this->registerGroupProvider();
-        $this->registerThrottleProvider();
-        $this->registerSession();
-        $this->registerCookie();
-        $this->registerSentry();
-    }
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->registerHasher();
+		$this->registerUserProvider();
+		$this->registerGroupProvider();
+		$this->registerThrottleProvider();
+		$this->registerSession();
+		$this->registerCookie();
+		$this->registerSentry();
+	}
 
-    /**
-     * Register the hasher used by Sentry.
-     *
-     * @return void
-     */
-    protected function registerHasher()
-    {
-        $this->app['sentry.hasher'] = $this->app->share(function($app)
-        {
-            $hasher = $app['config']['cartalyst/sentry::hasher'];
+	/**
+	 * Register the hasher used by Sentry.
+	 *
+	 * @return void
+	 */
+	protected function registerHasher()
+	{
+		$this->app['sentry.hasher'] = $this->app->share(
+			function ($app)
+			{
+				$hasher = $app['config']['cartalyst/sentry::hasher'];
 
-            switch ($hasher)
-            {
-                case 'native':
-                case 'bcrypt':
-                case 'sha256':
-                case 'whirlpool':
-                    $hasherName = ucfirst($hasher) . 'Hasher';
-                    return new $hasherName;
-            }
+				switch ($hasher)
+				{
+					case 'native':
+						return new Hashing\NativeHasher;
+						break;
 
-            throw new \InvalidArgumentException("Invalid hasher [$hasher] chosen for Sentry.");
-        });
-    }
+					case 'bcrypt':
+						return new Hashing\BcryptHasher;
+						break;
 
-    /**
-     * Register the user provider used by Sentry.
-     *
-     * @return void
-     */
-    protected function registerUserProvider()
-    {
-        $this->app['sentry.user'] = $this->app->share(function($app)
-        {
-            if (isset($this->app[UserProvider::class]))
-            {
-                return $this->app[UserProvider::class];
-            }
+					case 'sha256':
+						return new Hashing\Sha256Hasher;
+						break;
 
-            throw new \UnexpectedValueException("User provider not configured.");
-        });
-    }
+					case 'whirlpool':
+						return new Hashing\WhirlpoolHasher;
+						break;
+				}
 
-    /**
-     * Register the group provider used by Sentry.
-     *
-     * @return void
-     */
-    protected function registerGroupProvider()
-    {
-        $this->app['sentry.group'] = $this->app->share(function($app)
-        {
-            if (isset($this->app[GroupProvider::class]))
-            {
-                return $this->app[GroupProvider::class];
-            }
+				throw new \InvalidArgumentException("Invalid hasher [$hasher] chosen for Sentry.");
+			}
+		);
 
-            throw new \UnexpectedValueException("Group provider not configured.");
-        });
-    }
+		$this->app->bind(Hashing\HasherInterface::class, function (Container $app){
+			return $app['sentry.hasher'];
+		});
+	}
 
-    /**
-     * Register the throttle provider used by Sentry.
-     *
-     * @return void
-     */
-    protected function registerThrottleProvider()
-    {
-        $this->app['sentry.throttle'] = $this->app->share(function($app)
-        {
-            if (isset($this->app[ThrottleProvider::class]))
-            {
-                return $this->app[ThrottleProvider::class];
-            }
+	/**
+	 * Register the user provider used by Sentry.
+	 *
+	 * @return void
+	 */
+	protected function registerUserProvider()
+	{
+		$this->app['sentry.user'] = $this->app->share(function ($app){
+			if (isset($this->app[UserProvider::class]))
+			{
+				return $this->app[UserProvider::class];
+			}
 
-            throw new \UnexpectedValueException("Throttling provider not configured.");
-        });
-    }
+			throw new \UnexpectedValueException("User provider not configured.");
+		});
+	}
 
-    /**
-     * Register the session driver used by Sentry.
-     *
-     * @return void
-     */
-    protected function registerSession()
-    {
-        $this->app['sentry.session'] = $this->app->share(function($app)
-        {
-            $key = $app['config']['cartalyst/sentry::cookie.key'];
+	/**
+	 * Register the group provider used by Sentry.
+	 *
+	 * @return void
+	 */
+	protected function registerGroupProvider()
+	{
+		$this->app['sentry.group'] = $this->app->share(function ($app){
+			if (isset($this->app[GroupProvider::class]))
+			{
+				return $this->app[GroupProvider::class];
+			}
 
-            return new IlluminateSession($app['session.store'], $key);
-        });
-    }
+			throw new \UnexpectedValueException("Group provider not configured.");
+		});
+	}
 
-    /**
-     * Register the cookie driver used by Sentry.
-     *
-     * @return void
-     */
-    protected function registerCookie()
-    {
-        $this->app['sentry.cookie'] = $this->app->share(function($app)
-        {
-            $key = $app['config']['cartalyst/sentry::cookie.key'];
+	/**
+	 * Register the throttle provider used by Sentry.
+	 *
+	 * @return void
+	 */
+	protected function registerThrottleProvider()
+	{
+		$this->app['sentry.throttle'] = $this->app->share(function ($app){
+			if (isset($this->app[ThrottleProvider::class]))
+			{
+				return $this->app[ThrottleProvider::class];
+			}
 
-            /**
-             * We'll default to using the 'request' strategy, but switch to
-             * 'jar' if the Laravel version in use is 4.0.*
-             */
+			throw new \UnexpectedValueException("Throttling provider not configured.");
+		});
+	}
 
-            $strategy = 'request';
+	/**
+	 * Register the session driver used by Sentry.
+	 *
+	 * @return void
+	 */
+	protected function registerSession()
+	{
+		$this->app['sentry.session'] = $this->app->share(function ($app){
+			$key = $app['config']['cartalyst/sentry::cookie.key'];
 
-            if (preg_match('/^4\.0\.\d*$/D', $app::VERSION))
-            {
-                $strategy = 'jar';
-            }
+			return new IlluminateSession($app['session.store'], $key);
+		});
+	}
 
-            return new IlluminateCookie($app['request'], $app['cookie'], $key, $strategy);
-        });
-    }
+	/**
+	 * Register the cookie driver used by Sentry.
+	 *
+	 * @return void
+	 */
+	protected function registerCookie()
+	{
+		$this->app['sentry.cookie'] = $this->app->share(function ($app){
+			$key = $app['config']['cartalyst/sentry::cookie.key'];
 
-    /**
-     * Takes all the components of Sentry and glues them
-     * together to create Sentry.
-     *
-     * @return void
-     */
-    protected function registerSentry()
-    {
-        $this->app['sentry'] = $this->app->share(function($app)
-        {
-            return new Sentry(
-                $app['sentry.user'],
-                $app['sentry.group'],
-                $app['sentry.throttle'],
-                $app['sentry.session'],
-                $app['sentry.cookie'],
-                $app['request']->getClientIp()
-            );
-        });
-    }
+			/**
+			 * We'll default to using the 'request' strategy, but switch to
+			 * 'jar' if the Laravel version in use is 4.0.*
+			 */
+
+			$strategy = 'request';
+
+			if (preg_match('/^4\.0\.\d*$/D', $app::VERSION))
+			{
+				$strategy = 'jar';
+			}
+
+			return new IlluminateCookie($app['request'], $app['cookie'], $key, $strategy);
+		});
+	}
+
+	/**
+	 * Takes all the components of Sentry and glues them
+	 * together to create Sentry.
+	 *
+	 * @return void
+	 */
+	protected function registerSentry()
+	{
+		$this->app['sentry'] = $this->app->share(function ($app){
+			return new Sentry(
+				$app['sentry.user'],
+				$app['sentry.group'],
+				$app['sentry.throttle'],
+				$app['sentry.session'],
+				$app['sentry.cookie'],
+				$app['request']->getClientIp()
+			);
+		});
+	}
 }
