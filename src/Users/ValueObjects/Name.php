1093c0f125e47d05056a66030b17a51bbe440a4b
Guido Contreras Woda <gcontreras@digbang.com>
2015-08-04 12:46:08 -0300

    Default toString on Name

--- src/Users/ValueObjects/Name.php
+++ src/Users/ValueObjects/Name.php
@@ -49,4 +49,15 @@ class Name
 	{
 		return $this->firstName . $separator . $this->lastName;
 	}
+
+	/**
+	 * The __toString method allows a class to decide how it will react when it is converted to a string.
+	 *
+	 * @return string
+	 * @link http://php.net/manual/en/language.oop5.magic.php#language.oop5.magic.tostring
+	 */
+	public function __toString()
+	{
+		return $this->getFullName();
+	}
 }
